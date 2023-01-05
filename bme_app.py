9f3b6fd20c09bc263e0ff9861dc4733ad222bf3c
spandan13 <spandanathaide@gmail.com>
2023-01-05 10:38:58 +0530

    Create separate file for getting data

--- bme_app.py
+++ bme_app.py
@@ -1,32 +1,15 @@
 #!/bin/python3
 
-import requests
-import math
-from bme_data import BME280Module
+import bme_data_old
 from flask import Flask, render_template
 
-lat = "19.367738"
-lon = "72.7965333"
-api_key = "e6ab838e58e11e0a698c0a606a42d0ae"
 server_name = "OrbitSrv"
-
-def outdoors_weather():
-    weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}").json()
-    temp = (int(weather_data['main']['temp']-273.15))
-    pressure = (int(weather_data['main']['pressure']))
-    humidity = (int(weather_data['main']['humidity']))
-    
-    return humidity,pressure,temp
-
-bme280_module = BME280Module()
 
 app = Flask(__name__)
 @app.route("/")
 def index():
-    out_humidity,out_pressure,out_temp = outdoors_weather()
-    humidity,pressure,temp = bme280_module.get_sensor_readings()
-    altitude = 44330 * (1.0 - math.pow(pressure / out_pressure, 0.1903))
-    return render_template("index.html",server_name=server_name,o_hu=out_humidity,hu=round(humidity,2),o_pre=out_pressure,pre=round(pressure,2),o_temp=out_temp,temp=round(temp,2),alti=round(altitude,2))
+    owm_humidity,owm_pressure,owm_temp,bme_humidity,bme_pressure,bme_temp,altitude = bme_data_old.get_all_data()
+    return render_template("index.html",server_name=server_name,o_hu=owm_humidity,hu=round(bme_humidity,2),o_pre=owm_pressure,pre=round(bme_pressure,2),o_temp=owm_temp,temp=round(bme_temp,2),alti=round(altitude,2))
 
 app.run(host="0.0.0.0", port=6162)
 
--- bme_data_old.py
+++ bme_data_old.py
@@ -1,11 +1,18 @@
 import bme280
 import smbus2
+import requests
+import math
 
+# Config for BME280 Sensor
 port = 2
 address = 0x76 # Adafruit BME280 address. Other BME280s may be different
 bus = smbus2.SMBus(port)
-
 bme280.load_calibration_params(bus,address)
+
+# Config for OpenWeatherMap API
+lat = "19.367738"
+lon = "72.7965333"
+api_key = "e6ab838e58e11e0a698c0a606a42d0ae"
 
 def bme_data():
     bme280_data = bme280.sample(bus,address)
@@ -14,3 +21,18 @@ def bme_data():
     ambient_temperature = bme280_data.temperature
     
     return humidity, pressure, ambient_temperature
+
+def owm_data():
+    weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}").json()
+    temp = (int(weather_data['main']['temp']-273.15))
+    pressure = (int(weather_data['main']['pressure']))
+    humidity = (int(weather_data['main']['humidity']))
+    
+    return humidity, pressure, temp
+
+def get_all_data():
+    owm_humidity,owm_pressure,owm_temp = owm_data()
+    bme_humidity,bme_pressure,bme_temp = bme_data()
+    altitude = 44330 * (1.0 - math.pow(bme_pressure / owm_pressure, 0.1903))
+    
+    return owm_humidity,owm_pressure,owm_temp,bme_humidity,bme_pressure,bme_temp,altitude
