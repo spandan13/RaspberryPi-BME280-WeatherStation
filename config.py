c91c8db20b18ba355c43ed5ffd93d064605a95ba
spandan13 <spandanathaide@gmail.com>
2023-01-06 10:24:01 +0530

    Externalized settings to separate file

---
+++ config.py
@@ -0,0 +1,18 @@
+import configparser
+import ast
+
+config = configparser.ConfigParser()
+config.read("settings")
+
+owm_config = config['OpenWeatherMap']
+lat = owm_config['lat']
+lon = owm_config['lon']
+api_key = owm_config['api_key']
+
+bme_config = config['bme_sensor']
+port = int(bme_config['port'])
+address = ast.literal_eval(bme_config['address'])
+
+other = config['Other']
+server_name = other['server_name']
+db_file = other['db_file']
