83153e08f5cfb7b708224d0dbdbecead1e1d2743
spandan13 <spandanathaide@gmail.com>
2023-01-17 10:54:20 +0530

    Switched to WeatherAPI

--- log_to_db.py
+++ log_to_db.py
@@ -6,11 +6,11 @@ db_file = config.db_file
 conn=sqlite3.connect(db_file)
 curs=conn.cursor()
 # function to insert data on a table
-def add_data (owm_humidity,owm_pressure,owm_temp,bme_humidity,bme_pressure,bme_temp,altitude):
-    curs.execute("INSERT INTO weather_data values(datetime('now','localtime'), (?), (?), (?), (?), (?), (?), (?))",
-                 (owm_temp,bme_temp,owm_humidity,bme_humidity,owm_pressure,bme_pressure,altitude))
+def add_data (wapi_humidity,wapi_pressure,wapi_temp,wapi_wind,wapi_wind_dir,bme_humidity,bme_pressure,bme_temp):
+    curs.execute("INSERT INTO weather_data values(datetime('now','localtime'), (?), (?), (?), (?), (?), (?), (?), (?))",
+                 (wapi_temp,bme_temp,wapi_humidity,bme_humidity,wapi_pressure,bme_pressure,wapi_wind,wapi_wind_dir))
     conn.commit()
     conn.close()
     
-owm_humidity,owm_pressure,owm_temp,bme_humidity,bme_pressure,bme_temp,altitude = weather_data.get_all_data()
-add_data(owm_humidity,owm_pressure,owm_temp,round(bme_humidity,2),round(bme_pressure,2),round(bme_temp,2),round(altitude,2))
+wapi_humidity,wapi_pressure,wapi_temp,wapi_wind,wapi_wind_dir,bme_humidity,bme_pressure,bme_temp = weather_data.get_all_data()
+add_data(wapi_humidity,wapi_pressure,wapi_temp,wapi_wind,wapi_wind_dir,round(bme_humidity,2),round(bme_pressure,2),round(bme_temp,2))
