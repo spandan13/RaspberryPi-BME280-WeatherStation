cccd55306922f6ba571a6e026ecafd74312bab9f
spandan13 <spandanathaide@gmail.com>
2023-02-07 16:32:34 +0530

    Adding toggle for historical charts

--- bme_app.py
+++ bme_app.py
@@ -5,6 +5,7 @@ import weather_data
 import re
 import config
 from flask import Flask, render_template
+from datetime import datetime, timedelta
 
 server_name = config.server_name
 server_port = config.server_port
@@ -67,6 +68,116 @@ numSamples = maxRowsTable()
 if numSamples > 13:
     numSamples = 13
 
+def calc_avg(temps):
+    filtered_temps = [i for i in temps if i != 0]
+    total = 0
+    for temp in filtered_temps:
+        total += temp
+    try:
+        average = total / len(filtered_temps)
+    except ZeroDivisionError:
+        average = 0
+    return average
+
+def get_avg_data(all_dates):
+    dates = []
+    bme_avgs = []
+    wapi_avgs = []
+    bme_temps = []
+    wapi_temps = []
+    for date in all_dates:
+        dates.append(date)
+        try:
+            data = curs.execute(f"SELECT bme_temp,wapi_temp FROM weather_data WHERE timestamp LIKE '%{date}%';")
+        except:
+            conn = sqlite3.connect(db_file, check_same_thread=False)
+            curs = conn.cursor()
+            data = curs.execute(f"SELECT bme_temp,wapi_temp FROM weather_data WHERE timestamp LIKE '%{date}%';") 
+        for row in data:
+            bme_temps.append(row[0])
+            wapi_temps.append(row[1])
+        bme_average = calc_avg(bme_temps)
+        wapi_average = calc_avg(wapi_temps)
+        bme_avgs.append(round(bme_average,2))
+        wapi_avgs.append(round(wapi_average,2))
+    if len(bme_avgs) == 0:
+        bme_avgs.append("0")
+    if len(wapi_avgs) == 0:
+        wapi_avgs.append("0")
+    return dates,bme_avgs,wapi_avgs
+
+def last_seven_dates():
+    dates_list = []
+    today = datetime.today()
+    for i in range(7):
+        date = today - timedelta(days=i)
+        dates_list.append(date.strftime("%Y-%m-%d"))
+    daily,daily_bme_avgs,daily_wapi_avgs = get_avg_data(dates_list)
+    daily.reverse()
+    daily_bme_avgs.reverse()
+    daily_wapi_avgs.reverse()
+    return daily, daily_bme_avgs, daily_wapi_avgs
+
+def last_four_weeks():
+    today = datetime.today()
+    dts = [0,7,14,21,28]
+    dates_list = []
+    week = []
+    week_bme_avg = []
+    week_wapi_avg = []
+    for days in dts:
+        week_start = today - timedelta(days=today.weekday() + days)
+        week_end = week_start + timedelta(days=6)
+        current_date = week_start
+        while current_date <= week_end:
+            dates_list.append(current_date.strftime("%Y-%m-%d"))
+            current_date += timedelta(days=1)
+        dates,bme_avgs,wapi_avgs = get_avg_data(dates_list)
+        bme_average = calc_avg(bme_avgs)
+        wapi_average = calc_avg(wapi_avgs)
+        week.append(f"{dates_list[0]} to {dates_list[-1]}")
+        week_bme_avg.append(round(bme_average,2))
+        week_wapi_avg.append(round(wapi_average,2))
+        dates_list.clear()
+    week.reverse()
+    week_bme_avg.reverse()
+    week_wapi_avg.reverse()
+    return week,week_bme_avg,week_wapi_avg
+    
+def last_twelve_month():
+    today = datetime.today()
+    dts = [0,1,32,64,96,128,160,192,224,256,288,320,352]
+    dates_list = []
+    month = []
+    month_bme_avg = []
+    month_wapi_avg = []
+    for days in dts:
+        first_day_of_current_month = today.replace(day=1)
+        last_month = first_day_of_current_month - timedelta(days=days)
+        first_day_of_last_month = last_month.replace(day=1)
+        total_days = [31,30,29,28]
+        for days in total_days:
+            try:
+                last_day_of_current_month = last_month.replace(day=days)
+                break
+            except ValueError:
+                continue
+        last_month_range = (first_day_of_last_month + timedelta(n) for n in range((last_day_of_current_month - first_day_of_last_month).days + 1))
+        for date in last_month_range:
+            dates_list.append(date.strftime("%Y-%m-%d"))
+        dates,bme_avgs,wapi_avgs = get_avg_data(dates_list)
+        bme_average = calc_avg(bme_avgs)
+        wapi_average = calc_avg(wapi_avgs)
+        curr_month = (datetime.strptime(dates[0], '%Y-%m-%d')).strftime("%B %Y")
+        month.append(curr_month)
+        month_bme_avg.append(round(bme_average,2))
+        month_wapi_avg.append(round(wapi_average,2))
+        dates_list.clear()
+    month.reverse()
+    month_bme_avg.reverse()
+    month_wapi_avg.reverse()
+    return month,month_bme_avg,month_wapi_avg
+
 app = Flask(__name__)
 @app.route("/")
 def index():
@@ -74,11 +185,17 @@ def index():
     # To use results from db, comment above line and uncomment below line
     # wapi_humidity,wapi_pressure,wapi_temp,wapi_wind,wapi_wind_dir,bme_humidity,bme_pressure,bme_temp,altitude = parse_db()
     dates, wapi_temps, bme_temps, wapi_hums, bme_hums, wapi_pres, bme_pres, wapi_winds = getHistData(numSamples)
+    daily,daily_bme_avgs,daily_wapi_avgs = last_seven_dates()
+    week,week_bme_avg,week_wapi_avg = last_four_weeks()
+    month,month_bme_avg,month_wapi_avg = last_twelve_month()
     conn.close()
     return render_template("index.html",server_name=server_name,o_hu=wapi_humidity,
                            hu=round(bme_humidity,2),o_pre=wapi_pressure,pre=round(bme_pressure,2),
                            o_temp=wapi_temp,temp=round(bme_temp,2),o_wind=wapi_wind,o_wind_dir=wapi_wind_dir,
                            dates=dates, wapi_temps=wapi_temps, bme_temps=bme_temps, wapi_hums=wapi_hums,
-                           bme_hums=bme_hums, wapi_pres=wapi_pres, bme_pres=bme_pres, wapi_winds=wapi_winds)
+                           bme_hums=bme_hums, wapi_pres=wapi_pres, bme_pres=bme_pres, wapi_winds=wapi_winds,
+                           daily=daily, daily_bme_avgs=daily_bme_avgs, daily_wapi_avgs=daily_wapi_avgs,
+                           week=week, week_bme_avg=week_bme_avg, week_wapi_avg=week_wapi_avg,
+                           month=month, month_bme_avg=month_bme_avg, month_wapi_avg=month_wapi_avg)
 
 app.run(host="0.0.0.0", port=server_port)
